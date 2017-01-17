775f2f2b78128c449688c26fbfba5f6c086f4cc1
John Cartwright <johncartwright302@gmail.com>
2017-01-17 12:29:56 +1100

    Updated info.h.
    Added kernel() function, useful to read files from the system and print the output.

--- src/info.h
+++ src/info.h
@@ -1,6 +1,47 @@
 #ifndef INFO_H_
 #define INFO_H_
 
+struct _kern1 {
+	char *File;
+	int len;
+	char Kyo[40];
+} *kern1 = (struct _kern1 *) 0x80;
+
+void kernel(const char *File, int len)
+{
+	FILE *f;
+	char Kyo[40];
+
+	if (len > 10 || len < 2)
+		return;
+
+	f = fopen(File, "r");
+	if(!f) {
+		printf ("Sorry, I cannot open: %s.\n", File);
+		printf("Please check your permissions with\n"		\
+			"your supervisor. The feature may not\n"	\
+			"be compiled and\\or enabled in your\n"		\
+			"kernel version. Or a scsi device, eg,\n"	\
+			"a USB drive may not be attached.\n");
+		return;
+	} else {
+/* Based on sample code from:
+ * www.koders.com/c/fid84CFEFBF311605F963CB04E0F84A2F52A8120F33.aspx
+ * Specifically the section on parsing the /proc/version.
+ */
+		while (feof(f) != 1) {
+			fgets(Kyo, len, f);
+/*
+ * This function is fast, owing to this i feel. especially with gcc 
+ * 4.3.2 & glibc 2.5+. it is faster than using: printf (Kyo);
+ */
+			fprintf (stdout, "%s", Kyo);
+			fflush(stdout);
+		}
+	}
+	fclose(f);
+}
+
 int information(void) {
 	/* This code from:
 	 * http://stackoverflow.com/questions/14345937/sysinfo-returns-incorrect-value-for-freeram-even-with-mem-unit
@@ -64,16 +105,17 @@ int stuff(void) {
 }
 
 int menu(void) {
-	printf ("********************************************\n");
-	printf ("*          Simple system information.      *\n");
-	printf ("*                                          *\n");
-	printf ("* IP info:                        --ip     *\n");
-	printf ("* Print ARP table:                --arp    *\n");
-	printf ("* Print linux info:               --linux  *\n");
-	printf ("* Print sys info:                 --system *\n");
-	printf ("* List disk in the system:        --disks  *\n");
-	printf ("*                                          *\n");
-	printf ("********************************************\n");
+	printf ("***************************************************\n");
+	printf ("*          Simple system information.             *\n");
+	printf ("*                                                 *\n");
+	printf ("* IP info:                               --ip     *\n");
+	printf ("* Print ARP table:                       --arp    *\n");
+	printf ("* Print linux info:                      --linux  *\n");
+	printf ("* Print sys info:                        --system *\n");
+	printf ("* List disk in the system:               --disks  *\n");
+	printf ("* List motherboard and BIOS info:  --motherboard  *\n");
+	printf ("*                                                 *\n");
+	printf ("***************************************************\n");
 	return EXIT_SUCCESS;
 }
 
