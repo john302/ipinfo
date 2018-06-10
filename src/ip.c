b9b2c4c5b5c26a2aef58311312dcdc32d31ae91a
John Cartwright <johncartwright302@gmail.com>
2018-06-11 08:50:25 +1000

    Updated ip.c. Added a function call to return the TCP table.
    Calling a function to return the TCP table.

--- src/ip.c
+++ src/ip.c
@@ -1,12 +1,3 @@
-/********************************************************************
-* Description: System information utility.
-* Author: John Cartwright <johncartwright302@dodo.com.au>
-* Created at: Mon Jan 16 10:02:12 AEDT 2017
-* Computer: localhost.localdomain
-* System: Linux 4.8.15 on x86_64
-*
-********************************************************************/
-
 #include <linux/kernel.h>       /* for struct sysinfo */
 #include <sys/sysinfo.h>        /* For the sysinfo struct. */
 #include <stdio.h>
@@ -15,27 +6,23 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <sys/utsname.h>
+#include <sys/unistd.h>
 #include <sys/ioctl.h>
 #include <net/if.h>
 #include <stdlib.h> /* For the system() function. */
 #include <gnu/libc-version.h> /* Get the GNU Libc version. */
-#include <pwd.h>
-#include <sys/types.h>
-#include <ifaddrs.h>
+#include <netdb.h>
 
 #include "info.h"
+#include "iface.h"
 
 #define BUF 0x05
 
 int main (int argc, char **argv) {
 
 	char hostname[128];
-	struct passwd *passwd;
-	passwd = getpwuid ( getuid());
 
-	char* myarg1 = argv[1];
-
-	if (!argc || !myarg1) {
+	if (argv[1] == NULL) {
 		menu();
 	}
 
@@ -61,16 +48,17 @@ int main (int argc, char **argv) {
 	}
 
 	if (argc > 1 && strncmp (argv[1], "--ip", BUF) == 0) {
-		// Get the LAN IP address.
-		printf("LAN IP address.\n");
-		system("hostname -I | awk '{print $1}'");
-
+		printf("Printing the IP addresse(s) on the machine.\n");
+		printf("-----------------------------------------------------\n\n\n");
+                iface();
 		// Get the Internet facing IP address.
-		internetip();
+		printf("Printing the TCP table.\n");
+		printf("-----------------------------------------------------\n\n\n");
+//		internetip();
+		kernel("/proc/net/tcp", 2);
 	}
 
 	if (argc > 1 && strncmp (argv[1], "--disks", BUF) == 0) {
-
 		// List the hard disks attached to the system.
 		execl("/bin/lsblk", "/bin/lsblk", "-d",NULL);
 	}
@@ -78,26 +66,6 @@ int main (int argc, char **argv) {
 	if (argc > 1 && strncmp (argv[1], "--linux", BUF) == 0) {
 		// Print system information.
 		stuff();
-
-		fprintf(stdout, "\nLogin Name is: %s\n", passwd->pw_name);
-		fprintf(stdout, "Login shell is: %s\n", passwd->pw_shell);
-		fprintf(stdout, "User home is: %s\n", passwd->pw_dir);
-		fprintf(stdout, "User information: %s\n", passwd->pw_gecos);
-		printf("\nThe system has %lu CPU cores.\n", sysconf(_SC_NPROCESSORS_ONLN));
-		printf("The system has %lu pages of physical memory.\n", sysconf(_SC_PHYS_PAGES));
-		printf("The system has %lu pages of available physical memory.\n", sysconf(_SC_AVPHYS_PAGES));
-	}
-
-	if (argc > 1 && strncmp (argv[1], "--motherboard", BUF) == 0) {
-		printf("\t\tMotherboard & BIOS information.\n");
-		printf("--BIOS date: ");
-		kernel("/sys/class/dmi/id/bios_date", 4);
-		printf("--BIOS vendor: ");
-		kernel("/sys/class/dmi/id/bios_vendor", 4);
-		printf("--Motherboard name: ");
-		kernel("/sys/class/dmi/id/board_name", 4);
-		printf("--Motherboard vendor: ");
-		kernel("/sys/class/dmi/id/board_vendor", 4);
 	}
 
 	return 0;
